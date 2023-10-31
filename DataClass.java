84201f8c12b93cc5578cc8dd43d94d0dae3b6d8a
shruti patel <Shrutipatel203@gmail.com>
2023-10-31 19:18:41 +0000

    datatypecommit

---
+++ DataClass.java
@@ -0,0 +1,19 @@
+package Datapackage;
+
+public class DataClass {
+
+    public static void main(String[] args){
+
+        int A=40;
+        int B=30;
+
+        System.out.println(A+B);
+        System.out.println(A-B);
+        System.out.println(A*B);
+        System.out.println(A/B);
+        System.out.println(A++ );
+        System.out.println(B++ );
+
+
+    }
+}
